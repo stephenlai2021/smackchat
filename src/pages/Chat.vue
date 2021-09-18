7513fb8465f272cc3caff89d73085e9849c3560d
stephenlaichaowen <stephenlaitest@gmail.com>
2021-09-18 22:18:31 +0800

    commit

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -51,8 +51,8 @@
   
     <chat-messages />
 
-    <!-- <image-modal :file="file" @close-imageModal="file = null" /> -->
-    <image-modal v-if="file" @close-imageModal="file = null" />
+    <image-modal :file="file" @close-imageModal="file = null" />
+    <!-- <image-modal v-if="file" @close-imageModal="file = null" /> -->
 
     <map-modal v-if="showMapModal" @close-mapmodal="showMapModal = false" />
 
